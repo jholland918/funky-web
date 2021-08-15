eb4e1ac606119efa7fff3c48192c5346016d6145
DESKTOP-JR6P3DK\Jason <jholland918@gmail.com>
2021-08-15 08:54:03 -0500

    add cs proj

---
+++ FunkyWeb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunkyWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
